f127c937eae85b96de92b0b46e3f4de4a6e27e17
Donovan Young <dyoung522@gmail.com>
2012-07-16 11:52:38 -0400

    Clean rails install, no mods

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WwwDonovanyoungCom::Application.config.secret_token = 'c144435922ba6924c1c23ed381a3e8a8a3452975fb41cd4968bbf70b6febc1114e1f940143e31ff9cd342b955ab67dac26097fa3e492e15bd7c15768a9400143'
