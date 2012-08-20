15747f63219f9a235c97b4b99083118e9a5dda90
Donovan Young <dyoung522@gmail.com>
2012-08-20 15:33:19 -0400

    Code refactor

--- app/controllers/china_adventures_controller.rb
+++ app/controllers/china_adventures_controller.rb
@@ -2,6 +2,6 @@ class ChinaAdventuresController < ApplicationController
   # GET /china_adventures
   # GET /china_adventures.json
   def index
-    @china_adventures = ChinaAdventure.order("date asc").page(params[:page]).per(7)
+    @china_adventures = ChinaAdventure.page(params[:page]).per(7)
   end
 end
--- app/models/china_adventure.rb
+++ app/models/china_adventure.rb
@@ -1,4 +1,6 @@
 class ChinaAdventure < ActiveRecord::Base
   attr_accessible :content, :date, :title
   validates :title, :date, :content, presence: true
+
+  default_scope order("date asc")
 end
