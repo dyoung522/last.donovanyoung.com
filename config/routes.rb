acbb51593319aec910a2f73868239e0647d65449
Donovan Young <dyoung522@gmail.com>
2012-08-21 11:08:55 -0400

    Changed china_adventures route to simply china

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 DonovanYoung::Application.routes.draw do
-  resources :china_adventures, only: :index
+  # China Adventure
+  resources :china, controller: 'china_adventures', only: :index do
+    get ':page', action: :index, on: :collection
+  end
+  match '/china_adventures' => redirect('/china')
 
+  # Personal Webpages
   match '/about',  to: "me#about"
   match '/resume', to: "me#resume"
 
+  # Home Page
   root to: "me#home"
 end
