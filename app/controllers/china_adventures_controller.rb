e9ce53a1020831b4ca4166a72d656180e18afe4e
Donovan C. Young <dyoung522@gmail.com>
2013-12-15 14:05:51 -0500

    convert china_adventure to strong parameters

--- app/controllers/china_adventures_controller.rb
+++ app/controllers/china_adventures_controller.rb
@@ -2,6 +2,7 @@ class ChinaAdventuresController < ApplicationController
   # GET /china_adventures
   # GET /china_adventures.json
   def index
+    params.permit( :content, :date, :title )
     @china_adventures = ChinaAdventure.page(params[:page]).per(7)
   end
 end
